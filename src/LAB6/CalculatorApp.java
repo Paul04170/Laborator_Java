c36e9d359ff5066b6234efc76fbe3242b3fe8c07
Paul04170 <paulandrei.costea@ulbsibiu.ro>
2025-04-03 11:03:40 +0300

    112

--- src/LAB6/CalculatorApp.java
+++ src/LAB6/CalculatorApp.java
@@ -6,14 +6,14 @@ public class CalculatorApp {
         NewIntCalculator intCalculator = new NewIntCalculator();
         DoubleCalculator doubleCalculator = new DoubleCalculator();
 
-        // Operațiile pentru int: (10 + 5 - 3) * 2
+        // Operațiile pentru int:(10 + 5 - 3) * 2
         intCalculator.add(10);
         intCalculator.add(5);
         intCalculator.subtract(3);
         intCalculator.multiply(2);
         System.out.println("Rezultatul pentru int: " + intCalculator.result());  // Așteptat: 24
 
-        // Operațiile pentru double: (10 + 5 - 3.3) * 2.2
+        // Operațiile pentru double:(10 + 5 - 3.3) * 2.2
         doubleCalculator.add(10);
         doubleCalculator.add(5);
         doubleCalculator.subtract(3.3);
